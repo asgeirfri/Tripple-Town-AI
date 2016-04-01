f2db0b223653e1df0da1a708f08ac9e79c356f36
Quang Nguyen <Kwa@Quangs-MacBook-Pro.local>
2016-04-01 19:24:27 +0000

    aiUI

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,32 @@ import java.awt.Point;
 
 public class Main {
 	
-	public static TripleTownUI board = new TripleTownUI();
+	
 	
 	public static void main(String[] args){
-		HumanGame();
-		//AIGame();
+		//HumanGame();
+		AIGame();
 
 	}
 	
 	public static void AIGame() {
-		TTAgent agent = new TTAgent();
+		TripleTownUI boardUI = new TripleTownUI();
 		TTBoard board = new TTBoard();
 		board.init();
-		board.show();
-		while (!board.gameOver()) {
-			/*if (agent.stash()) {
-				board.stash();
-			}
-			else {*/
-			try {
-				Point move = agent.nextMove(board);
-				System.out.println("Making move " + move);
-				board.playerMove(move);
-				board.moveBears();
-			}
-			catch (RuntimeException e) {
-				System.out.println("Invalid move.\n" + e.getMessage());
-			}
-			//}
-			System.out.println("------------------");
-			board.show();
-			
+		if(boardUI.isVisible() == false) {
+			boardUI.setVisible(true);
+		}
+		
+		while(!board.gameOver()) {
+			boardUI.StartButton.doClick();
 		}
 	}
 	public static void HumanGame() {
-		if(board.isVisible() == false) {
-			board.setVisible(true);
+		TripleTownUI boardUI = new TripleTownUI();
+		TTBoard board = new TTBoard();
+		board.init();
+		if(boardUI.isVisible() == false) {
+			boardUI.setVisible(true);
 		}
 	}
 }
