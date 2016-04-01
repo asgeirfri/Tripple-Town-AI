e72006b04868bacff8e271917961cd872d4d6be8
RoyalCookie <janusthorkristjansson@gmail.com>
2016-04-01 19:39:41 +0000

    Adding Images

--- src/Main.java
+++ src/Main.java
@@ -14,34 +14,12 @@ public class Main {
 		TripleTownUI boardUI = new TripleTownUI();
 		TTBoard board = new TTBoard();
 		board.init();
-<<<<<<< HEAD
-		board.show();
-		while (!board.gameOver()) {
-			/*if (agent.stash()) {
-				board.stash();
-			}
-			else {*/
-			//try {
-				Point move = agent.nextMove(board);
-				System.out.println("Making move " + move);
-				board.playerMove(move);
-				board.moveBears();
-			//}
-			//catch (RuntimeException e) {
-				//System.out.println("Invalid move.\n" + e.getMessage());
-			//}
-			//}
-			System.out.println("------------------");
-			board.show();
-			
-=======
 		if(boardUI.isVisible() == false) {
 			boardUI.setVisible(true);
 		}
 		
 		while(!board.gameOver()) {
 			boardUI.StartButton.doClick();
->>>>>>> f2db0b223653e1df0da1a708f08ac9e79c356f36
 		}
 	}
 	public static void HumanGame() {
